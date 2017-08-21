7d6ccdfc68694f5fa135a17958cb37e8b89b9fac
RedPer <aleksander.wiecaszek@gmail.com>
2017-08-21 19:06:25 +0200

    First commit. First version of the mod.

---
+++ src/main/java/redper/minecraft/autosprint/handler/KeyHandler.java
@@ -0,0 +1,65 @@
+package redper.minecraft.autosprint.handler;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent.ClientTickEvent;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.util.Color;
+
+public class KeyHandler {
+
+    private KeyBinding toggleSprintKey;
+    private boolean toggled = false;
+    private Minecraft mc;
+    private int sprintKey;
+    private FontRenderer fontRenderer;
+
+    public KeyHandler() {
+
+        toggleSprintKey = new KeyBinding("key.autosprint.toggle", Keyboard.KEY_R, "key.categories.movement");
+        ClientRegistry.registerKeyBinding(toggleSprintKey);
+
+        mc = Minecraft.getMinecraft();
+        sprintKey = mc.gameSettings.keyBindSprint.getKeyCode();
+        fontRenderer = mc.fontRenderer;
+
+    }
+
+    @SubscribeEvent
+    public void onTick(ClientTickEvent event) {
+
+        sprintKey = mc.gameSettings.keyBindSprint.getKeyCode();
+
+        if(toggleSprintKey.isPressed()) {
+
+            toggled = !toggled;
+
+            if(!toggled) {
+                KeyBinding.setKeyBindState(sprintKey, false);
+            }
+
+        }
+
+        if(toggled) {
+            KeyBinding.setKeyBindState(sprintKey, true);
+        }
+
+    }
+
+    @SubscribeEvent
+    public void onRender(RenderGameOverlayEvent event) {
+
+        if(toggled && event.getType() == RenderGameOverlayEvent.ElementType.TEXT) {
+
+            int color = Integer.parseInt("aaffaa", 16);
+            mc.fontRenderer.drawString("Sprint [Toggled]", 5, 5, color);
+
+        }
+
+    }
+
+}
