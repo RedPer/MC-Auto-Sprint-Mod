5bf0b2fccd5ff665b98f6d19607e5267483b111c
RedPer <aleksander.wiecaszek@gmail.com>
2017-08-26 16:54:12 +0200

    Added Configuration. Added saving sprint state (so for example you exited Minecraft with sprint enabled and you run it one again you have sprint enabled). Splitted Text Renderer and Key Handler. Added text color changing (you can current do this only through changing value in file autosprintmod.cfg located in .minecraft directory/config)

--- 1.12/src/main/java/redper/minecraft/autosprint/AutoSprintMod.java
+++ 1.12/src/main/java/redper/minecraft/autosprint/AutoSprintMod.java
@@ -1,29 +1,86 @@
 package redper.minecraft.autosprint;
 
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.common.config.Property;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import redper.minecraft.autosprint.handler.KeyHandler;
+import redper.minecraft.autosprint.render.SprintRenderer;
+
+import java.io.File;
+import java.util.TreeMap;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION, acceptedMinecraftVersions = Reference.MC_VERSIONS, clientSideOnly = true, canBeDeactivated = true)
 public class AutoSprintMod {
 
+    private Configuration config;
+
     @Mod.Instance(Reference.MOD_ID)
     private static AutoSprintMod instance;
 
+    public Configuration getConfig() { return config; }
+
+    public static AutoSprintMod getInstance() { return instance; }
+
     public AutoSprintMod() {
 
+        File confFile = new File(Loader.instance().getConfigDir(), "autosprintmod.cfg");
+        config = new Configuration(confFile);
+
+        syncConfig(true);
+
+    }
+
+    private void syncConfig(boolean load) {
+
+        if (!config.isChild && load) {
+            config.load();
+        }
+
+        Property prop;
+
+        prop = config.get(Configuration.CATEGORY_CLIENT, "isSprintToggled", false);
+        prop.setComment("Determines if sprint is toggled.");
+        prop.setLanguageKey("config.client.sprinttoggled");
+        prop.setRequiresMcRestart(true);
+        prop.setRequiresWorldRestart(false);
+        prop.setShowInGui(false);
+
+        prop = config.get(Configuration.CATEGORY_CLIENT, "hexTextColor", "aaffaa");
+        prop.setComment("The Hex Color Of All The text displayed");
+        prop.setLanguageKey("config.client.textcolor");
+        prop.setRequiresMcRestart(false);
+        prop.setRequiresWorldRestart(false);
+        prop.setShowInGui(true);
+
+        if(config.hasChanged()) {
+            config.save();
+        }
+
+    }
+
+    @SubscribeEvent
+    public void onConfigChanged(ConfigChangedEvent.OnConfigChangedEvent event) {
+        if(event.getModID().equals(Reference.MOD_ID)) {
+            syncConfig(false);
+        }
     }
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
 
+        MinecraftForge.EVENT_BUS.register(this);
+
         if(event.getSide() == Side.CLIENT) {
-            MinecraftForge.EVENT_BUS.register(new KeyHandler());
+            MinecraftForge.EVENT_BUS.register(KeyHandler.getInstance());
+            MinecraftForge.EVENT_BUS.register(SprintRenderer.getInstance());
         }
 
     }
